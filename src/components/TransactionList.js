287c40bb25e724e5c5fd0684d0b94d2e1397daaf
bcast201 <bcast201@gmail.com>
2021-04-22 21:40:43 -0500

    main commit

---
+++ src/components/TransactionList.js
@@ -0,0 +1,22 @@
+import React, {useContext } from 'react';
+import Transaction from './Transaction';
+import { GlobalContext } from '../context/GlobalState'
+
+const TransactionList = () => {
+    const { transactions }= useContext(GlobalContext);
+
+
+    return (
+        <>     
+            {transactions.length > 0 &&
+                <h3>History</h3>
+            }
+                
+            <ul className='list'>
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+            </ul>
+        </>
+    )
+}
+
+export default TransactionList
